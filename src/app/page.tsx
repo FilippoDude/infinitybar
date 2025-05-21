c85836703897810a4aedc2438de3c5e69381ada8
FilippoDude <filippogrochala@gmail.com>
2025-05-21 20:52:24 +0200

    First version

---
+++ src/app/page.tsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <> 
+      <section className="relative w-full h-[100lvh] flex items-center justify-center z-10">
+        <div className="absolute w-full h-full filter brightness-50">
+          <Image alt="Background image" src={"/homeBackground.png"} fill={true}></Image>
+        </div>
+        <div className="relative w-150 h-40">
+          <Image alt="Background image" src={"/homeTitle.png"} fill={true}></Image>
+        </div>
+        <nav className="absolute top-0 left-0 h-16 w-full bg-[#141415d0] flex items-center justify-center">
+          <div className="absolute left-10 w-32 h-6"><Image alt="Logo" src={"/logo.png"} fill={true}></Image></div>
+          <button className="absolute right-10 text-xl font-sans text-[#D9A166] font-bold">CONTATTACI</button>
+          <div className="flex flex-row gap-6">
+            <button className="font-rajdhani-sans text-2xl text-white">Menu</button>
+            <button className="font-rajdhani-sans text-2xl text-white">Sul bar</button>
+            <button className="font-rajdhani-sans text-2xl text-white">Su di noi</button>
+          </div>
+        </nav>
+        {//<div className="absolute items-center bottom-2 flex flex-col">
+          //<p className=" text-xl font-sans text-white opacity-60 font-normal">Aperti dalle 8 alle 22</p>
+        //</div>
+        }
+      </section>
+      <section className="relative w-full py-10 bg-[#141415]">
+        <div className="relative w-full h-full flex items-center justify-center gap-10">
+          <div className="relative w-225 h-150 filter brightness-50">
+            <Image alt="Bar Image" src={"/aboutBackground.png"} fill={true}></Image>
+          </div>
+          <div className="flex flex-col gap-10">
+            <div className="rounded-2xl overflow-hidden relative w-70 h-70 filter brightness-50">
+              <Image alt="Bar Image" src={"/aboutBackground.png"} fill={true}></Image>
+            </div>
+            <div className="rounded-2xl overflow-hidden relative w-70 h-70 filter brightness-50">
+              <Image alt="Bar Image" src={"/aboutBackground.png"} fill={true}></Image>
+            </div>
+          </div>
+        </div>
+        <div className="relative w-full bg-[#141415] flex flex-col items-center gap-10 mt-10">
+          <div className="relative h-fit w-fit flex flex-row gap-5">
+            <div className="relative w-150 h-100 border-2 border-[#E89947]">
+              <Image alt="Pasticcini Image" src={"/pasticciniBackground.png"} fill={true}></Image>
+            </div>
+            <div className="relative w-150 h-100">  
+              <h1 className=" text-5xl font-sans text-white font-semibold italic">I nostri pasticcini</h1>
+              <p className=" text-3xl font-sans text-white opacity-75 font-semibold italic">Pasticcini e pasticcine sono tutti presenti</p>
+            </div>
+          </div>
+      
+          <div className="relative h-fit w-fit flex flex-row gap-5">
+            <div className="relative w-150 h-100">
+              <h1 className=" text-5xl font-sans text-white font-semibold italic">Gelati e granite</h1>
+              <p className=" text-3xl font-sans text-white opacity-75 font-semibold italic">Granitini e granitine sono tutti presenti</p>
+            </div>
+            <div className="relative w-150 h-100  border-2 border-[#E89947]">  
+              <Image alt="Pasticcini Image" src={"/gelatiBackground.png"} fill={true}></Image>
+            </div>
+          </div>
+          <div className="relative h-fit w-fit flex flex-row gap-5">
+            <div className="relative w-150 h-100 border-2 border-[#E89947]">
+              <Image alt="Pasticcini Image" src={"/rosticceriaBackground.png"} fill={true}></Image>
+            </div>
+            <div className="relative w-150 h-100">  
+              <h1 className="text-5xl font-sans text-white font-semibold italic">Rosticceria calda</h1>
+              <p className="text-3xl font-sans text-white opacity-75 font-semibold italic">Rosticcerini e rosticcerine sono tutti presenti</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer className="relative w-full max-h-60 h-60 bg-[#232325]">
+        <p className="ml-10 text-2xl font-sans text-white opacity-75 font-normal">Il bar Infinity</p>
+      </footer>
+    </> 
+  );
+}
